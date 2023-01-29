eef2b1795848786ce0901ae558752e18439b4449
EC2 Default User <ec2-user@ip-172-31-25-139.ap-northeast-1.compute.internal>
2023-01-29 12:05:10 +0000

    読みやすいコードにするため、メソッドを作成して整理、不要な要素の削除、インデントの調整

--- janken.rb
+++ janken.rb
@@ -1,67 +1,63 @@
-hands = ["グー", "チョキ", "パー"]
-directions = ["上", "下", "右", "左"]
-janken_result = ""
+def janken
+  hands = ["グー", "チョキ", "パー"]
+  puts "じゃんけん..."
+  puts "[0]グー　[1]チョキ　[2]パー"
+  player = gets.to_i
+  program = rand(3)
+  puts "ぽん！"
+  puts "あなた：#{hands[player]} VS 相手：#{hands[program]}"
+
+  while player == program
+    puts "-----------------------"
+    puts "あいこで..."
+    puts "[0]グー　[1]チョキ　[2]パー"
+    player = gets.to_i
+    program = rand(3)
+    puts "しょ！"
+    puts "あなた：#{hands[player]} VS 相手：#{hands[program]}"
+  end
+
+  if (player == 0 && program == 1) || (player == 1 && program == 2) || (player == 2 && program == 0)
+    puts "\e[41mじゃんけんに勝ちました！\e[0m"
+    return true
+  else
+    puts "\e[44mじゃんけんに負けました\e[0m"
+  end
+end
+
+def acchimuite_hoi
+  directions = ["上", "下", "左", "右"]
+  puts "あっちむいて..."
+  puts "[0]上　[1]下　[2]左　[3]右"
+  player = gets.to_i
+  program = rand(4)
+  puts "ほい！"
+  puts "あなた：#{directions[player]} VS 相手：#{directions[program]}"
+  puts "-----------------------"
+  if player == program
+    return true
+  end
+end
 
 loop {
+  round1 = janken
+  round2 = acchimuite_hoi
 
-puts "じゃんけん..."
-puts "[0]グー　[1]チョキ　[2]パー"
-player_hand = hands[gets.to_i]
-program_hand = hands[rand(3)]
-puts "ぽん！"
-puts "あなた：#{player_hand} VS 相手：#{program_hand}"
-
-while player_hand == program_hand do
-  puts "あいこで..."
-  puts "[0]グー　[1]チョキ　[2]パー"
-  player_hand = hands[gets.to_i]
-  program_hand = hands[rand(3)]
-  puts "しょ！"
-  puts "あなた：#{player_hand} VS 相手：#{program_hand}"
-end
-
-if player_hand == hands[0] && program_hand == hands [1] ||
-   player_hand == hands[1] && program_hand == hands [2] ||
-   player_hand == hands[2] && program_hand == hands [0]
-     janken_result = "じゃんけんに勝ちました！" 
-else janken_result = "じゃんけんに負けました！"
-end
-
-case janken_result
-when "じゃんけんに勝ちました！"
-  puts(janken_result)
-  puts "あっちむいて..."
-  puts "[0]上　[1]下　[2]左　[3]右"
-  player_direction = directions[gets.to_i]
-  program_direction = directions[rand(4)]
-  puts "ほい！"
-  puts "あなた:#{player_direction} VS 相手：#{program_direction}"
-  if player_direction == program_direction
-    puts "\e[41mあなたの勝ち！\e[0m"
-    puts "もう一度勝負しますか？"
-    puts "[0]はい　[1]いいえ"
-    if gets.to_i == 1
-      puts "また遊んでね！"
-      break
-    end
+  if round2 != true
+    next
   end
-when "じゃんけんに負けました！"
-  puts(janken_result)
-  puts "あっちむいて..."
-  puts "[0]上　[1]下　[2]左　[3]右"
-  player_direction = directions[gets.to_i]
-  program_direction = directions[rand(4)]
-  puts "ほい！"
-  puts "あなた:#{player_direction} VS 相手：#{program_direction}"
-  if player_direction == program_direction
-    puts "\e[44mあなたの負け！\e[0m"
-    puts "もう一度勝負しますか？"
-    puts "[0]はい　[1]いいえ"
-    if gets.to_i == 1
-      puts "また遊んでね！"
-      break
-    end
-  end
-end
 
+  if round1 == true
+    puts "\e[41mあなたの勝ち！おめでとう♪\e[0m"
+  else
+    puts "\e[44m残念、あなたの負けです\e[0m"
+  end
+
+  puts "もう一度勝負しますか？"
+  puts "[0]はい　[1]いいえ"
+  if gets.to_i == 1
+    puts "また遊んでね！"
+    break
+  end
 }
+
